09277e3b124981697a151d28507c02496a8d6499
answoals11 <56918380+answoals11@users.noreply.github.com>
2019-11-21 21:24:59 +0900

    loginpage

--- lifeshare/src/pages/Login/index.tsx
+++ lifeshare/src/pages/Login/index.tsx
@@ -2,6 +2,18 @@ import * as React from "react";
 
 import * as S from "./style";
 
-const Login: React.FC = () => <div>Login</div>;
+const Login: React.FC = () => (
+    
+    <S.LoginContainer>
+        <S.Div>
+       <S.InputId placeholder="ID" type="textbox"></S.InputId>
+
+        <S.InputPs placeholder="Password" type="password"></S.InputPs>
+    
+       <S.Button type="button">Login</S.Button>
+       </S.Div>
+    </S.LoginContainer>
+    
+);
 
 export default Login;
--- lifeshare/src/pages/Login/style.ts
+++ lifeshare/src/pages/Login/style.ts
@@ -1,1 +1,67 @@
 import styled from "styled-components";
+
+export const Div = styled.div`
+    display: inline-block;
+
+    margin:0 392px;
+
+`;
+
+export const LoginContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;  
+
+  margin: 0 392px;
+  height: 100vh;
+  
+`;
+
+export const InputPs = styled.input`
+    
+    all: unset;
+
+    margin-top: 5px;
+    margin-bottom: 60px;
+
+    border-bottom: 1px solid #bcbcbc;
+
+    
+    width: 392px;
+    height: 30px;
+    padding: 12px 4px;
+    font-size: 24px;
+    font-weight: 300;
+    background-color: white;
+    text-align: left;
+    padding-bottom: 0.5rem;
+    padding: 10px;
+`;
+
+export const InputId = styled.input`
+  all: unset;
+
+  border-bottom: 1px solid #bcbcbc;
+
+  width: 392px;
+  height: 30px;
+  padding: 12px 4px;
+  font-size: 24px;
+  font-weight: 300;
+  background-color: white;
+  text-align: left;
+  padding-bottom: 0.5rem;
+  padding: 10px;
+`;
+
+
+export const Button = styled.button`
+
+  width: 400px;
+  height: 48px;
+  background-color: #e5980b;
+  font-size: 24px;
+  color: white;
+  text-align:center;
+  border-radius: 5px;
+`;
